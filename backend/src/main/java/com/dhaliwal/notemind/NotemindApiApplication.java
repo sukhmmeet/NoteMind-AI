5bbe1e8bb3016d9c602aa7751e476d2b985406ff
Sukhmeet Singh Dhaliwal <144631710+dhaliwal918@users.noreply.github.com>
2026-04-19 15:16:13 +0530

    feat: initialize project with Spring Boot backend

---
+++ backend/src/main/java/com/dhaliwal/notemind/NotemindApiApplication.java
@@ -0,0 +1,13 @@
+package com.dhaliwal.notemind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotemindApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotemindApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dhaliwal/notemind/NotemindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhaliwal.notemind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotemindApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
